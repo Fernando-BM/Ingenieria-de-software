a780ea36bc4ef5e6a6c150250965408948e3109b
Fernando-BM <fercho@ciencias.unam.mx>
2019-02-05 17:13:53 -0600

    Agregando pojo e inversa

--- base/mapita.sql
+++ base/mapita.sql
@@ -5,14 +5,15 @@ CREATE TABLE usuario (idUsuario serial NOT NULL,
 		CORREO text NOT NULL, 
 		CONTRASENIA text NOT NULL,
 		FECHANACIMIENTO date,
-		PRIMARY KEY (idUsuaruio)
+		PRIMARY KEY (idUsuario)
 );
 
 CREATE TABLE marcador (idMarcador serial NOT NULL,
 		descrpcion text NOT NULL,
 		longitud text NOT NULL, 
 		latitud text NOT NULL,
-		idUsuario serial NOT NULL
+		idUsuario serial NOT NULL,
+		PRIMARY KEY (idMarcador)
 );
 
 INSERT INTO usuario (nomre,correo,contrasenia,fechanacimiento) VALUES ('Dani','dani@hotmail.es','171717','16-02-1994');
